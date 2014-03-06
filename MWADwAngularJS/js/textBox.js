e7f768794d9dab6437e0242afbbc063883a77c60
Brian Wu <brianwu02@gmail.com>
2014-03-06 17:37:21 -0500

    fixed undefined error for remaining amount by catching TypeError

--- MWADwAngularJS/js/textBox.js
+++ MWADwAngularJS/js/textBox.js
@@ -4,9 +4,30 @@ angular.module('SampleApp', [])
     MAX_LEN = 100;
     WARN_THRESHOLD = 50;
 
+
+    $scope.messages = [ {data: 'sample message'} ];
+
+    $scope.send = function(message_count) {
+        // send message to message list.
+        $scope.messages.splice(1, 0, { no: message_count , data: $scope.message });
+        // clear the text 
+        $scope.message = '';
+    };
+
+    $scope.clear = function() {
+        $scope.message = '';
+    };
+
+    $scope.remove = function(index) {
+        $scope.messages.splice(index, 1);
+    };
+
     $scope.remaining = function() {
-        messageLength = $scope.message.length;
-        return MAX_LEN - messageLength;
+        try {
+            return MAX_LEN - $scope.message.length;
+        } catch (TypeError) {
+            return 100;
+        }
     };
 
     $scope.shouldWarn = function() {
@@ -14,9 +35,8 @@ angular.module('SampleApp', [])
     };
 
     $scope.hasValidLength = function() {
-        if ($scope.message.length <= MAX_LEN) {
-            return true;
-        }
+        return true;
     };
+
     
     });
