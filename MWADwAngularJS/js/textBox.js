3cfa3f5a482b4445401e3bd10fa38959d497ff37
Brian Wu <brianwu02@gmail.com>
2014-03-06 19:40:08 -0500

    added service using factory method

--- MWADwAngularJS/js/textBox.js
+++ MWADwAngularJS/js/textBox.js
@@ -1,16 +1,18 @@
-angular.module('SampleApp', [])
-    .controller('TextAreaWithLimitControl', function($scope) {
 
+var sampleApp = angular.module('sampleApp', []);
+
+sampleApp.controller('TextAreaWithLimitControl', function($scope, helloWorldFactory) {
     MAX_LEN = 100;
     WARN_THRESHOLD = 50;
 
     $scope.messages = [];
     $scope.message = '';
 
-    $scope.send = function(message_count) {
+    $scope.send = function() {
         // send message to message list.
         $scope.messages.splice(1, 0, { 
-            data: $scope.message });
+            data: $scope.message 
+        });
         // clear the text 
         $scope.clear();
     };
@@ -41,5 +43,24 @@ angular.module('SampleApp', [])
             }
     };
 
-    
-    });
+    $scope.doFactoryMethod1 = function() {
+        // set the text box to data from factory method
+        $scope.message = helloWorldFactory.sayHello();
+    };
+
+    $scope.doFactoryMethod2 = function() {
+        $scope.message = helloWorldFactory.sayByeBye();
+    };
+});
+
+sampleApp.factory('helloWorldFactory', function() {
+    return {
+        sayHello: function() {
+            return "hello world!";
+        },
+
+        sayByeBye: function() {
+            return "bye bye!";
+        }
+    };
+});
