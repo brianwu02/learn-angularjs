908274e60f9defbdba878e68f045e8e7fd9d35c5
Brian Wu <brianwu02@gmail.com>
2014-03-08 13:19:28 -0500

    4->2 indenting

--- MWADwAngularJS/js/textBox.js
+++ MWADwAngularJS/js/textBox.js
@@ -2,65 +2,62 @@
 var sampleApp = angular.module('sampleApp', []);
 
 sampleApp.controller('TextAreaWithLimitControl', function($scope, helloWorldFactory) {
-    MAX_LEN = 100;
-    WARN_THRESHOLD = 50;
-
-    $scope.messages = [];
+  MAX_LEN = 100;
+  WARN_THRESHOLD = 50;
+  
+  $scope.messages = [];
+  $scope.message = '';
+  
+  $scope.send = function() {
+    // send message to message list.
+    $scope.messages.splice(1, 0, {
+      data: $scope.message 
+    });
+    // clear the text 
+    $scope.clear();
+  };
+  
+  $scope.clear = function() {
     $scope.message = '';
+  };
+  
+  $scope.remove = function(index) {
+    $scope.messages.splice(index, 1);
+  };
 
-    $scope.send = function() {
-        // send message to message list.
-        $scope.messages.splice(1, 0, { 
-            data: $scope.message 
-        });
-        // clear the text 
-        $scope.clear();
-    };
+  $scope.remaining = function() {
+    return MAX_LEN - $scope.message.length;
+  };
 
-    $scope.clear = function() {
-        $scope.message = '';
-    };
+  $scope.shouldBeRed = function() {
+    return $scope.remaining() < WARN_THRESHOLD;
+  };
 
-    $scope.remove = function(index) {
-        $scope.messages.splice(index, 1);
-    };
+  $scope.shouldBeGreen = function() {
+    return $scope.remaining() >= WARN_THRESHOLD;
+  };
 
-    $scope.remaining = function() {
-        return MAX_LEN - $scope.message.length;
-    };
-
-    $scope.shouldBeRed = function() {
-        return $scope.remaining() < WARN_THRESHOLD;
-    };
-
-    $scope.shouldBeGreen = function() {
-        return $scope.remaining() >= WARN_THRESHOLD;
-    };
-
-    $scope.hasValidLength = function() {
-            if ($scope.remaining() > 0){
-                return true;
-            }
-    };
-
-    $scope.doFactoryMethod1 = function() {
-        // set the text box to data from factory method
-        $scope.message = helloWorldFactory.sayHello();
-    };
-
-    $scope.doFactoryMethod2 = function() {
-        $scope.message = helloWorldFactory.sayByeBye();
-    };
+  $scope.hasValidLength = function() {
+    if ($scope.remaining() > 0) {
+      return true;
+    }
+  };
+  $scope.doFactoryMethod1 = function() {
+    // set the text box to data from factory method
+    $scope.message = helloWorldFactory.sayHello();
+  };
+  $scope.doFactoryMethod2 = function() {
+    $scope.message = helloWorldFactory.sayByeBye();
+  };
 });
 
 sampleApp.factory('helloWorldFactory', function() {
-    return {
-        sayHello: function() {
-            return "hello world!";
-        },
-
-        sayByeBye: function() {
-            return "bye bye!";
-        }
-    };
+  return {
+    sayHello: function() {
+      return "hello world!";
+    },
+    sayByeBye: function() {
+      return "bye bye!";
+    }
+  };
 });
